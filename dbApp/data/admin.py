366e6c79e4ae9d3cd03e65a9af7f3f533e87a973
Nishant Gaurav <nishantgaurav@icloud.com>
2015-10-21 14:56:17 +0530

    working models

--- dbApp/data/admin.py
+++ dbApp/data/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import Client
+from .models import Client, RegularUser, Page, ProUser, Disease, Event, Record, Review
 
 # Define an inline admin descriptor for Employee model
 class ClientInline(admin.StackedInline):
@@ -13,6 +13,37 @@ class ClientInline(admin.StackedInline):
 class UserAdmin(UserAdmin):
   inlines = (ClientInline, )
 
+class RegularUserAdmin(admin.ModelAdmin):
+    pass
+#    search_fields = ('category', )
+#    list_display = ('category', 'domain',)
+#    list_filter = ('domain',)
+
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+class ProUserAdmin(admin.ModelAdmin):
+    pass
+
+class DiseaseAdmin(admin.ModelAdmin):
+    pass
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+class RecordAdmin(admin.ModelAdmin):
+    pass
+
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(RegularUser, RegularUserAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(ProUser, ProUserAdmin)
+admin.site.register(Disease, DiseaseAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Record, RecordAdmin)
+admin.site.register(Review, ReviewAdmin)
--- dbApp/data/models.py
+++ dbApp/data/models.py
@@ -4,46 +4,48 @@ from django.contrib.auth.models import User
 class Client(models.Model):
     user = models.OneToOneField(User, primary_key=True)
     pro = models.BooleanField(default=False)
+    pic = models.ImageField(null=True, blank=True, upload_to='user_images')
 
 class RegularUser(models.Model):
-    client = models.ForiegnKey(Client, null=False, blank=False)
+    client = models.ForeignKey(Client, null=False, blank=False)
     date_of_birth = models.DateField(blank=False, null=False, verbose_name="DOB")
     male_gender = models.BooleanField(default=False)
 
 class Page(models.Model):
-    name = models.CharField(blank=False, null=False, verbose_name="Diaplay Name")
-    verified = modls.BooleanField(default=False)
+    name = models.CharField(max_length=20, blank=False, null=False, verbose_name="Diaplay Name")
+    verified = models.BooleanField(default=False)
+    image = models.ImageField(null=True, blank=True, upload_to='page_images')
 
 class ProUser(models.Model):
-    client = models.ForiegnKey(Client, null=False, blank=False)
+    client = models.ForeignKey(Client, null=False, blank=False)
     specialization_details = models.TextField(max_length = 256,
         default='', help_text="Qualifications or Specialization of the Doctor. If the account is for an Institute then, provide the running departments.",
         verbose_name="Specialization Details")
     page = models.OneToOneField(Page, null=True, blank=True)
 
 class Disease(models.Model):
-    common_name = models.CharField(blank=False, null=False, verbose_name="Diaplay Name")
-    medical_name = models.CharField(blank=False, null=False, verbose_name="Medical Name")
+    common_name = models.CharField(max_length=20, blank=False, null=False, verbose_name="Diaplay Name")
+    medical_name = models.CharField(max_length=20, blank=False, null=False, verbose_name="Medical Name")
     description = models.TextField(max_length = 512,
         default='', help_text="placeholder for symptoms and prevention measures")
 
 class Event(models.Model):
     start_date = models.DateField(blank=False, null=False, verbose_name="Start Date")
     end_date = models.DateField(blank=False, null=False, verbose_name="End Date")
-    long = models.DecimalField(max_digits=8, decimal_places=3)
-    lat = models.DecimalField(max_digits=8, decimal_places=3)
-    disease = models.ForiegnKey(Disease, null=False, blank=False)
+    long = models.DecimalField(null=False, blank=False, max_digits=8, decimal_places=3, verbose_name="Longitude")
+    lat = models.DecimalField(null=False, blank=False, max_digits=8, decimal_places=3, verbose_name="Latitude")
+    disease = models.ForeignKey(Disease, null=False, blank=False)
     client = models.ForeignKey(RegularUser, null=False, blank=False)
 
 class Record(models.Model):
-    timestamp = models.DateTimeField(editable = True, auto_now_add=True)
-    event = models.ForiegnKey(Event, null=False, blank=False)
-    page = models.ForiegnKey(null=True, blank=True)
+    timestamp = models.DateTimeField(null=False, blank=False)
+    event = models.ForeignKey(Event, null=False, blank=False)
+    page = models.ForeignKey(Page, null=True, blank=True)
 
 class Review(models.Model):
     author = models.ForeignKey(RegularUser, null=False, blank=False)
     comments = models.TextField(max_length = 512, default='')
     date = models.DateTimeField(editable = True, auto_now=True)
     page = models.ForeignKey(Page, null=False, blank=False)
-    disease = models.ForeignKey(Page, null=False, blank=False)
+    disease = models.ForeignKey(Disease, null=False, blank=False)
     rating = models.PositiveSmallIntegerField(null=False, blank=False)
